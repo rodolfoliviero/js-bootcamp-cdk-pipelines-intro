d21c7dca471507dcc5240969c77c85ea32eb84f7
Rodolfo Liviero <48021+rodolfoliviero@users.noreply.github.com>
2022-01-11 12:22:25 -0300

    Update Github repo

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -5,7 +5,7 @@ import { CdkpipelinesDemoPipelineStack } from "../lib/cdkpipelines-demo-pipeline
 const app = new App();
 
 new CdkpipelinesDemoPipelineStack(app, "CdkpipelinesDemoPipelineStack", {
-  env: { account: "ACCOUNT", region: "us-east-1" },
+  env: { account: "811627709515", region: "us-east-1" },
 });
 
 app.synth();
